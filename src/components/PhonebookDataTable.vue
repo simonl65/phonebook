d4336412155adb023653b7744797e2912ddbdb2c
Simon Lincoln <simon.lincoln@contentdiscovered.com>
2018-12-06 16:11:04 +0000

    wip: Replace PhonebookDataTable component with repo's.

--- src/components/PhonebookDataTable.vue
+++ src/components/PhonebookDataTable.vue
@@ -1,193 +1,190 @@
 <template>
   <div>
-
-    <v-dialog
-      v-model="dialog"
-      max-width="500px">
-      <v-btn
-        slot="activator"
-        color="primary"
-        dark
-        class="mb-2">New Contact</v-btn>
+    <v-dialog v-model="dialog" max-width="500px">
+      <v-btn slot="activator" color="primary" dark class="mb-2">New Contact</v-btn>
       <v-card>
         <v-card-title>
           <span class="headline">{{ formTitle }}</span>
         </v-card-title>
-
         <v-card-text>
           <v-container grid-list-md>
             <v-layout wrap>
-              <v-flex
-                xs12
-                sm6
-                md4>
-                <v-text-field
-                  v-model="editedItem.name"
-                  label="Dessert name"/>
+              <v-flex xs12 sm6 md4>
+                <v-text-field v-model="editedItem.fullName" label="Full Name"></v-text-field>
               </v-flex>
-              <v-flex
-                xs12
-                sm6
-                md4>
-                <v-text-field
-                  v-model="editedItem.calories"
-                  label="Calories"/>
-              </v-flex>
-              <v-flex
-                xs12
-                sm6
-                md4>
-                <v-text-field
-                  v-model="editedItem.fat"
-                  label="Fat (g)"/>
-              </v-flex>
-              <v-flex
-                xs12
-                sm6
-                md4>
-                <v-text-field
-                  v-model="editedItem.carbs"
-                  label="Carbs (g)"/>
-              </v-flex>
-              <v-flex
-                xs12
-                sm6
-                md4>
-                <v-text-field
-                  v-model="editedItem.protein"
-                  label="Protein (g)"/>
+              <v-flex xs12 sm6 md4>
+                <v-text-field v-model="editedItem.tel" label="Phone"></v-text-field>
               </v-flex>
             </v-layout>
           </v-container>
         </v-card-text>
-
         <v-card-actions>
-          <v-spacer/>
-          <v-btn
-            color="blue darken-1"
-            flat
-            @click="close">Cancel</v-btn>
-          <v-btn
-            color="blue darken-1"
-            flat
-            @click="save">Save</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" flat @click.native="close">Cancel</v-btn>
+          <v-btn color="blue darken-1" flat @click.native="save">Save</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
 
+    <v-text-field v-model="search" append-icon="search" label="Search" single-line hide-details></v-text-field>
+
     <v-data-table
       :headers="headers"
       :items="contacts"
-      class="elevation-1" >
-      <template
-        slot="items"
-        slot-scope="props">
-        <td>{{ props.item.id }}</td>
+      :search="search"
+      :pagination.sync="pagination"
+      hide-actions
+      class="elevation-1"
+    >
+      <template slot="items" slot-scope="props">
         <td class="text-xs-right">{{ props.item.fullName }}</td>
         <td class="text-xs-right">{{ props.item.tel }}</td>
         <td class="justify-center layout px-0">
-          <v-icon
-            small
-            class="mr-2"
-            @click="editItem(props.item)">
-            edit
-          </v-icon>
-          <v-icon
-            small
-            @click="deleteItem(props.item)">
-            delete
-          </v-icon>
+          <v-btn icon class="mx-0" @click="editItem(props.item)">
+            <v-icon color="teal">edit</v-icon>
+          </v-btn>
+          <v-btn icon class="mx-0" @click="deleteItem(props.item)">
+            <v-icon color="pink">delete</v-icon>
+          </v-btn>
         </td>
       </template>
       <template slot="no-data">
-        <v-btn
-          color="primary"
-          @click="initialize">Reset</v-btn>
+        <v-btn color="primary" @click="initialize">Reset</v-btn>
       </template>
     </v-data-table>
+
+    <div class="text-xs-center pt-2">
+      <v-pagination v-model="pagination.page" :length="pages"></v-pagination>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      dialog: false,
-      headers: [
-        { text: 'ID', align: 'left', value: 'id' },
-        { text: 'Name', value: 'fullName' },
-        { text: 'Phone', value: 'tel' },
-        { text: 'Actions', value: 'name', sortable: false }
-      ],
-      contacts: [],
-      editedIndex: -1,
-      editedItem: {
-        id      : 0,
-        fullName: '',
-        tel     : '',
-      },
-      defaultItem: {
-        id      : 0,
-        fullName: '',
-        tel     : '',
-      }
-    }),
+export default {
+  data: () => ({
+    search: "",
+    pagination: { rowsPerPage: 5 },
+    dialog: false,
+    headers: [
+      { text: "Full Name", value: "fullName" },
+      { text: "Phone", value: "tel" },
+      { text: "Actions", value: "id", sortable: false }
+    ],
+    contacts: [],
+    editedIndex: -1,
+    editedItem: {
+      fullName: "",
+      tel: ""
+    },
+    defaultItem: {
+      fullName: "",
+      tel: ""
+    }
+  }),
 
-    computed: {
-      formTitle () {
-        return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
-      }
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? "New Contact" : "Edit Contact";
     },
 
-    watch: {
-      dialog (val) {
-        val || this.close()
+    pages() {
+      if (
+        this.pagination.rowsPerPage == null ||
+        this.pagination.totalItems == null
+      ) {
+        return 0;
       }
+
+      return Math.ceil(
+        this.pagination.totalItems / this.pagination.rowsPerPage
+      );
+    }
+  },
+
+  watch: {
+    dialog(val) {
+      val || this.close();
+    }
+  },
+
+  created() {
+    this.initialize();
+  },
+
+  methods: {
+    fetchContacts() {
+      axios.get("/contact").then(response => {
+        console.log(response);
+        this.contacts = response.data.data;
+      });
     },
 
-    created () {
-      this.initialize()
+    initialize() {
+      this.fetchContacts();
     },
 
-    methods: {
-      initialize() {
-        this.fetchContacts;
-      },
+    editItem(item) {
+      this.editedIndex = this.contacts.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
 
-      fetchContacts() {
-        axios.get('/contact')
-        .then( response => {
-          console.log(response);
-          this.contacts = response.data.data;
-        })
-      },
+    deleteItem(item) {
+      console.log(item);
 
-      editItem (item) {
-        this.editedIndex = this.contacts.indexOf(item)
-        this.editedItem = Object.assign({}, item)
-        this.dialog = true
-      },
+      const index = this.contacts.indexOf(item);
 
-      deleteItem (item) {
-        const index = this.contacts.indexOf(item)
-        confirm('Are you sure you want to delete this item?') && this.contacts.splice(index, 1)
-      },
+      confirm("Are you sure you want to delete this item?") &&
+        this.contacts.splice(index, 1);
 
-      close () {
-        this.dialog = false
-        setTimeout(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        }, 300)
-      },
+      console.log("deleted data");
 
-      save () {
-        if (this.editedIndex > -1) {
-          Object.assign(this.contacts[this.editedIndex], this.editedItem)
-        } else {
-          this.contacts.push(this.editedItem)
-        }
-        this.close()
+      axios.delete("/contact/" + item.id).then(response => {
+        console.log(response);
+      });
+    },
+
+    close() {
+      this.dialog = false;
+      setTimeout(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      }, 300);
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        console.log("edited data");
+        console.log(this.editedItem);
+
+        axios
+          .put("/contact/" + this.editedItem.id, {
+            name: this.editedItem.fullName,
+            phone: this.editedItem.tel
+          })
+          .then(response => {
+            console.log(response);
+          });
+
+        Object.assign(this.contacts[this.editedIndex], this.editedItem);
+      } else {
+        console.log("created data");
+        console.log(this.editedItem);
+
+        axios
+          .post("/contact", {
+            name: this.editedItem.fullName,
+            phone: this.editedItem.tel
+          })
+          .then(response => {
+            console.log(response);
+          });
+
+        this.contacts.push(this.editedItem);
       }
+
+      this.close();
     }
   }
+};
 </script>
